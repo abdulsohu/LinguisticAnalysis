ef40cb7d00be7481f979b7a8adb38b22156f9df2
abdulmoizmunir <sohu@sas.upenn.edu>
2020-12-21 21:25:02 -0500

    basic code

---
+++ race.py
@@ -0,0 +1,36 @@
+import parselmouth
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+mysp = __import__("my-voice-analysis")
+
+sns.set() # Use seaborn's default style to make attractive graphs
+plt.rcParams['figure.dpi'] = 100 # Show nicely large images in this notebook
+
+snd = parselmouth.Sound("audio/the_north_wind_and_the_sun.wav")
+
+
+plt.figure()
+plt.plot(snd.xs(), snd.values.T)
+plt.xlim([snd.xmin, snd.xmax])
+plt.xlabel("time [s]")
+plt.ylabel("amplitude")
+plt.show() # or plt.savefig("sound.png"), or plt.savefig("sound.pdf")
+
+
+def draw_spectrogram(spectrogram, dynamic_range=70):
+    X, Y = spectrogram.x_grid(), spectrogram.y_grid()
+    sg_db = 10 * np.log10(spectrogram.values)
+    plt.pcolormesh(X, Y, sg_db, vmin=sg_db.max() - dynamic_range, cmap='afmhot')
+    plt.ylim([spectrogram.ymin, spectrogram.ymax])
+    plt.xlabel("time [s]")
+    plt.ylabel("frequency [Hz]")
+
+def draw_intensity(intensity):
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=3, color='w')
+    plt.plot(intensity.xs(), intensity.values.T, linewidth=1)
+    plt.grid(False)
+    plt.ylim(0)
+    plt.ylabel("intensity [dB]")
